9c507e746c11886bdef62153644b933fe492b3ff
rzamirzayev <rzatm@code.edu.az>
2024-07-05 14:56:11 +0400

    SqlServerConnect

--- OganiWebUI/Program.cs
+++ OganiWebUI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OganiWebUI.Models.Contexts;
+
 namespace OganiWebUI
 {
     public class Program
@@ -5,7 +8,14 @@ namespace OganiWebUI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"));
+            });
             var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(name:"default",pattern:"{controller=home}/{action=index}/{id?}");
 
             app.MapGet("/", () => "Hello World!");
 

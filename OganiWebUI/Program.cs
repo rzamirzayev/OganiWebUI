fc3db2ff6481f831f42648578d649e1e025b5a4c
rzamirzayev <rzatm@code.edu.az>
2024-07-05 14:38:49 +0400

    createProject

---
+++ OganiWebUI/Program.cs
@@ -0,0 +1,15 @@
+namespace OganiWebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}

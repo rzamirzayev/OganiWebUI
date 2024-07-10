669d0a00c863b415b30e65e19d98498e5b80ab88
rzamirzayev <rzatm@code.edu.az>
2024-07-11 01:42:44 +0400

    encrypt decrypt complete

--- OganiWebUI/AppCode/Services/Implementation/CryptoService.cs
+++ OganiWebUI/AppCode/Services/Implementation/CryptoService.cs
@@ -6,95 +6,61 @@ using System.Web;
 
 namespace OganiWebUI.AppCode.Services.Implementation
 {
-    public class CryptoService :  ICryptoService
+    public class CryptoService : ICryptoService, IDisposable
     {
-        private CryptoServiceConfiguration options;
+        private readonly CryptoServiceConfiguration options;
+        private readonly HashAlgorithm ha;
+        private readonly SymmetricAlgorithm csp;
+        private bool disposed = false;
 
-        public CryptoService(IOptions<CryptoServiceConfiguration> options) { 
-        this.options=options.Value;
-                }
-        public string Decrypt(string chiperText)
+
+        public CryptoService(IOptions<CryptoServiceConfiguration> options)
         {
-            using (var symProvider = new TripleDESCryptoServiceProvider())
-            using (var md5 = MD5.Create())
-            {
-                byte[] valueBuffer = Convert.FromBase64String(chiperText);
-                byte[] keyBuffer = md5.ComputeHash(Encoding.ASCII.GetBytes($"{options.Key}202$"));
-                byte[] ivBuffer = md5.ComputeHash(Encoding.ASCII.GetBytes($"202$_#$@{options.Key}"));
-                var transformer = symProvider.CreateDecryptor(keyBuffer, ivBuffer);
-                using (var ms = new MemoryStream())
-                using (var cs = new CryptoStream(ms, transformer, CryptoStreamMode.Write))
-                {
-                    cs.Write(valueBuffer, 0, valueBuffer.Length);
-                    cs.FlushFinalBlock();
-                    ms.Position = 0;
-                    ms.Seek(0, SeekOrigin.Begin);
-                    byte[] bytes = new byte[ms.Length];
-                    ms.Read(bytes, 0, bytes.Length);
-                    string pureText = Encoding.UTF8.GetString(bytes);
-                    return pureText;
-                }
+            this.options = options.Value;
+            ha = MD5.Create();
+            csp = TripleDES.Create();
 
-
-            }
+            var keyBytes = ha.ComputeHash(Encoding.UTF8.GetBytes($"{this.options.Key}202$"));
+            var newKeyBytes = new byte[24];
+            Array.Copy(keyBytes, newKeyBytes, Math.Min(keyBytes.Length, newKeyBytes.Length));
+            csp.Key = newKeyBytes;
+            csp.Mode = CipherMode.ECB;
+            csp.Padding = PaddingMode.PKCS7;
+        }
+        public string Decrypt(string cipherText)
+        {
+            var cipherBytes = Convert.FromBase64String(cipherText);
+            var decryptedBytes = csp.CreateDecryptor().TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+            return Encoding.UTF8.GetString(decryptedBytes);
         }
 
-        public string Encrypt(string value,bool appliedUrlEncode=false)
+        public string Encrypt(string value, bool appliedUrlEncode = false)
         {
-            using(var symProvider=new TripleDESCryptoServiceProvider()) 
-            using(var md5=MD5.Create())
+            var valueBytes = Encoding.UTF8.GetBytes(value);
+            var cipherBytes = csp.CreateEncryptor().TransformFinalBlock(valueBytes, 0, valueBytes.Length);
+            var cipherText = Convert.ToBase64String(cipherBytes);
+            if (appliedUrlEncode)
             {
-                byte[] valueBuffer=Encoding.UTF8.GetBytes(value);
-                byte[] keyBuffer = md5.ComputeHash(Encoding.UTF8.GetBytes($"{options.Key}202$"));
-                byte[] ivBuffer = md5.ComputeHash(Encoding.UTF8.GetBytes($"202$_#$@{options.Key}"));
-                var transformer=symProvider.CreateEncryptor(keyBuffer, ivBuffer);
-                using(var ms=new MemoryStream())
-                using (var cs = new CryptoStream(ms, transformer,CryptoStreamMode.Write)) { 
-                    cs.Write(valueBuffer,0,valueBuffer.Length);
-                    cs.FlushFinalBlock();
-                    ms.Position = 0;
-                    ms.Seek(0, SeekOrigin.Begin);
-                    byte[] bytes = new byte[ms.Length];
-                    ms.Read(bytes,0, bytes.Length);
-                    string chiperText=Convert.ToBase64String(bytes);
-                    if (appliedUrlEncode)
-                    {
-                        chiperText = HttpUtility.UrlEncode(chiperText);
-                    }
-                    return chiperText;
-                }
-                 
-
+                cipherText = HttpUtility.UrlEncode(cipherText);
             }
-            //using(var provider=new TripleDESCryptoServiceProvider())
-            //using(var md5 = MD5.Create())
-            //{
-            //    try
-            //    {
-            //        var keyBuffer=md5.ComputeHash(Encoding.UTF8.GetBytes($"#{options.Key}!2024"));
-            //        var ivBuffer = md5.ComputeHash(Encoding.UTF8.GetBytes($"2024${options.Key}$"));
-            //        var transform =provider.CreateEncryptor(keyBuffer, ivBuffer);
-            //        using (var ms=new MemoryStream())
-            //        using (var cs=new CryptoStream(ms,transform,CryptoStreamMode.Write))
-            //        {
-            //            var textBuffer = Encoding.UTF8.GetBytes(value);
-            //            cs.Write(textBuffer,0, textBuffer.Length);
-            //            cs.FlushFinalBlock();
-            //            ms.Position = 0;
-            //            var result = new byte[ms.Length];
-            //            ms.Read(result,0,result.Length);
-            //            var chiperText=Convert.ToBase64String(result);
-            //            if (appliedUrlEncode)
-            //                return HttpUtility.UrlEncode(chiperText);
+            return cipherText;
+        }
 
-            //            return chiperText;
-            //        }
-            //    }
-            //    catch
-            //    {
-            //        return "";
-            //    }
-            //}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                ha.Dispose();
+                csp.Dispose();
+            }
+            disposed = true;
         }
     }
 }
